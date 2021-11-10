614f52a2283717321f81528e0615f7f2e8513582
ikerfc <ikerfc02@gmail.com>
2021-11-10 13:25:56 +0100

    1847.03 Añadido numero de paginas

--- Libro.java
+++ Libro.java
@@ -12,14 +12,16 @@ public class Libro {
 
     private String autor;
     private String titulo;
-
+    private int numeroPaginas;
+    
     /**
      * Fija el autor y el titulo del libro a los dados como parametro
      */
-    public Libro(String autorLibro, String tituloLibro)
+    public Libro(String autorLibro, String tituloLibro, int numeroDePaginas)
     {
         autor = autorLibro;
         titulo = tituloLibro;
+        numeroPaginas = numeroDePaginas;
     }
     
     /**
@@ -48,5 +50,13 @@ public class Libro {
      */
     public void imprimeTitulo(){
         System.out.println(titulo);
+    }
+    
+    /**
+     * Devuelve el numero de paginas
+     */
+    
+    public int getNumeroPaginas(){
+        return numeroPaginas;
     }
 }
